9177a87f46a6b56d97a33005686ed80b0adefd38
shina439 <limnyuyshina@gmail.com>
2025-07-10 15:26:25 +0100

    to do list

--- day 19 DOM/index.js
+++
@@ -1,6 +0,0 @@
-window.loading = () => {
-    const heading = document.createElement("h1");
-    const headingText = document.createTextNode("big head!");
-    heading.appendChild(headingText);
-    document.body.appendChild(heading);
-}
