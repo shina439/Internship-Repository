817d106075b3ae0263deeb1fd9d7813d42ec6f32
shina439 <limnyuyshina@gmail.com>
2025-07-10 15:53:48 +0100

    added some css tyles

--- day 19 DOM/script.js
+++ day 19 DOM/script.js
@@ -15,12 +15,23 @@ const inputBox = document.getElementById("input-box");
             li.appendChild(span);
         }
         inputBox.value = "";
+        saveData();
     }
     listContainer.addEventListener("click", function (e){
         if(e.target.tagName === "LI"){
-            e.target.classList.toggle("cheked");
+            e.target.classList.toggle("checked");
+            saveData();
         }
         else if(e.target.tagName === "SPAN"){
             e.target.parentElement.remove();
+            saveData();
         }
     }, false);
+
+    function saveData(){
+        localStorage.setItem("data", listContainer.innerHTML);
+    }
+    function showTask(){
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
+    showTask();
